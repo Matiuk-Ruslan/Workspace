dba8309220b2ff709786f24df2d886ae6f9f5ae1
ruslan.matiuk <ruslan.matiuk@maib.md>
2023-04-20 16:32:28 +0300

    section

---
+++ packages/Sandbox/Schemas/RealtyPage/RealtyPage.js
@@ -0,0 +1,177 @@
+define("RealtyPage", [], function() {
+	return {
+		entitySchemaName: "Realty",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "RealtyFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "Realty"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{
+			"Notes": {
+				"73d0a0be-7f81-45fd-88a2-20fa0c49716e": {
+					"uId": "73d0a0be-7f81-45fd-88a2-20fa0c49716e",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 0,
+					"property": 2,
+					"logical": 0,
+					"conditions": [
+						{
+							"comparisonType": 7,
+							"leftExpression": {
+								"type": 1,
+								"attribute": "Price"
+							},
+							"rightExpression": {
+								"type": 0,
+								"value": 100000,
+								"dataValueType": 5
+							}
+						}
+					]
+				}
+			}
+		}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Name",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "Name"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Area",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "Area"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Price",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "Price"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "RealtyType",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "RealtyType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "OfferType",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "OfferType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Notes",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "Notes"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": [],
+					"order": 0
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Files",
+				"values": {
+					"itemType": 2
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 0
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
---
+++ packages/Sandbox/Schemas/RealtySection/RealtySection.js
@@ -0,0 +1,8 @@
+define("RealtySection", [], function() {
+	return {
+		entitySchemaName: "Realty",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
