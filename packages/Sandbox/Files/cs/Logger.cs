96857948348249603069157bf77899dbd8646672
ruslan.matiuk <ruslan.matiuk@maib.md>
2023-04-20 14:34:02 +0300

    logger

---
+++ packages/Sandbox/Files/cs/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terrasoft.Core.DB;
+using Terrasoft.Core;
+
+namespace Sandbox.cs
+{
+    public static class Logger
+    {
+        public static bool WriteToLog(string service, string body, UserConnection userConnection)
+        {
+            try
+            {
+                Insert insert = new Insert(userConnection).Into("UsrLog")
+                    .Set("Execution", Column.Parameter(DateTime.Now))
+                    .Set("Service", Column.Parameter(service))
+                    .Set("Body", Column.Parameter(body));
+                insert.Execute();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
